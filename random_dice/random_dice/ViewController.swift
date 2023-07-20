1a0a9d4735998aacbfabc49c64a21f3ea31c96df
funMango <bluemango608@gmail.com>
2023-07-20 10:06:03 +0900

    random_dice(function): view random dice

--- random_dice/random_dice/ViewController.swift
+++ random_dice/random_dice/ViewController.swift
@@ -8,14 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let diceImgs = ["black1","black2","black3","black4","black5"]
+    
+    @IBOutlet var diceImgView_right: UIImageView!
+    @IBOutlet var diceImgView_left: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImgView_left.image = UIImage(named: diceImgs[0])
+        diceImgView_right.image = UIImage(named: diceImgs[0])
     }
-
     
-    
+    @IBAction func roll(_ sender: UIButton) {
+        let diceImg_left = diceImgs[Int.random(in: 0..<5)]
+        let diceImg_right = diceImgs[Int.random(in: 0..<5)]
+        diceImgView_left.image = UIImage(named: diceImg_left)
+        diceImgView_right.image = UIImage(named: diceImg_right)
+    }
 }
-
