6adcec43b61dd181d235c889185898fd8cf6f295
funMango <bluemango608@gmail.com>
2023-07-21 10:47:30 +0900

    BingoGame(fix): entire logic change

--- BingoGame/BingoGame/ViewController.swift
+++ BingoGame/BingoGame/ViewController.swift
@@ -12,42 +12,43 @@ class ViewController: UIViewController {
     @IBOutlet var lblNumber: UILabel!
     @IBOutlet var lblResult: UILabel!
     
-    var computer : Int? = 0
-    var myNumber : Int? = 0
+    var computer = Int.random(in: 1...10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblNumber.text = "선택하세요"
-        lblResult.text = ""
-        computer = Int.random(in: 1...10)
+        lblNumber.text = ""
+        lblResult.text = "선택하세요"
     }
     
     @IBAction func btn_number(_ sender: UIButton) {
         guard let selected = sender.currentTitle else {return}
         lblNumber.text = selected
-        myNumber = Int(selected)
     }
     
     @IBAction func btn_reset(_ sender: UIButton) {
         computer = Int.random(in: 1...10)
-        lblNumber.text = "선택하세요"
-        lblResult.text = ""
+        lblNumber.text = ""
+        lblResult.text = "선택하세요"
     }
 
     @IBAction func btn_select(_ sender: UIButton) {
-        if let number = myNumber {
-            lblNumber.text = String(number)
-        } else { return }
+        guard let numString = lblNumber.text else {
+            lblResult.text = "숫자를 입력해 주세요"
+            return
+        }
         
-        if let computer = computer, let myNumber = myNumber {
-            if computer > myNumber {
-                lblResult.text = "UP"
-            } else if computer < myNumber {
-                lblResult.text = "DOWN"
-            } else {
-                lblResult.text = "BINGO"
-            }
-        } else { return }
+        guard let myNumber = Int(numString) else {
+            lblResult.text = "숫자를 입력해 주세요"
+            return
+        }
+        
+        if computer > myNumber {
+            lblResult.text = "UP"
+        } else if computer < myNumber {
+            lblResult.text = "DOWN"
+        } else {
+            lblResult.text = "BINGO"
+        }
     }
 }
 
