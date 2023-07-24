1c6fff63167eb15ff14ee7d96d9c1e4cb691f166
funMango <bluemango608@gmail.com>
2023-07-24 17:12:15 +0900

    Timer(function): make timer function

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblSecond: UILabel!
+    @IBOutlet var lblStartSec: UILabel!
+    @IBOutlet var lblFinishSec: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var btn_start: UIButton!
+        
+    var timer: Timer?
+    var counter = 0
+    var timerStart = false
+    var isPaused = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slider.minimumValue = 0.0
+        slider.maximumValue = 60.0
+        slider.setValue(30.0, animated: true)
     }
-
-
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let seconds = Int(slider.value)
+        counter = seconds
+        lblSecond.text = "\(seconds) 초"
+    }
+    
+    @IBAction func btnReset(_ sender: UIButton) {
+        timerStart = false
+        isPaused = false
+        counter = 0
+        lblSecond.text = "초를 선택하세요"
+        slider.setValue(30.0, animated: true)
+        stopTimer()
+    }
+    
+    @IBAction func btnStart(_ sender: UIButton) {
+        if counter == 0 {
+            lblSecond.text = "초를 선택해야 시작할 수 있습니다."
+            AudioServicesPlaySystemSound(SystemSoundID(1000))
+        } else {
+            if timerStart {
+                pauseTimer()
+                isPaused = true
+                timerStart = false
+                btn_start.setTitle("START", for: .normal)
+            } else {
+                if isPaused {
+                    resumeTimer()
+                    isPaused = false
+                } else {
+                    startTimer()
+                }
+                timerStart = true
+                btn_start.setTitle("PAUSE", for: .normal)
+            }
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerTick), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerTick() {
+        // 타이머가 호출할 메서드입니다. 이 메서드는 1초마다 호출됩니다.
+        counter -= 1
+        slider.setValue(Float(counter), animated: true)
+        lblSecond.text = "\(counter) 초"
+        
+        if counter == 0 {
+            stopTimer()
+        }
+    }
+    
+    func pauseTimer() {
+        if timer != nil && timer!.isValid {
+            timer?.invalidate()
+            isPaused = true
+        }
+    }
+    
+    func resumeTimer() {
+        if isPaused {
+            isPaused = false
+            startTimer()
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        isPaused = false
+        AudioServicesPlaySystemSound(SystemSoundID(1000))
+    }
 }
 
