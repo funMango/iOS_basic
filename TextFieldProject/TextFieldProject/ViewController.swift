bfe26df0380e7cba424ea14acfa08a9c5a24f73d
funMango <bluemango608@gmail.com>
2023-07-24 18:45:15 +0900

    TextFieldProject(func): set up textField

--- TextFieldProject/TextFieldProject/ViewController.swift
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -17,8 +17,10 @@ class ViewController: UIViewController {
     
     func setUp() {
         view.backgroundColor = UIColor.gray
-        
-        textField.keyboardType = UIKeyboardType.numberPad
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "email을 입력해 주세요"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
     }
 
     @IBAction func btnDoneTapped(_ sender: UIButton) {
