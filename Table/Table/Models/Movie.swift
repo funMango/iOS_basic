d24b7e615808006ca985a87d5ee3383ed4ef5133
funMango <bluemango608@gmail.com>
2023-08-02 22:31:07 +0900

    Table(logic): add movie source

---
+++ Table/Table/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Table
+//
+//  Created by 이민호 on 2023/08/02.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImg: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
