1bbfc2e7dc82b7cc770a79432b6c4b9784ee815c
funMango <bluemango608@gmail.com>
2023-08-04 13:52:54 +0900

    TableCode: first commit

--- Table/Table/Controllers/ViewController.swift
+++ Table/Table/Controllers/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UIViewController {
         tableView.delegate = self
         
         movieDataManager.makeMovieData()
+        
+        title = "영화목록"
     }
     
     
---
+++ TableCode/TableCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableCode
+//
+//  Created by 이민호 on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
