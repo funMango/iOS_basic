145469099f7e292f0ccd073f413f58ee4e09f760
funMango <bluemango608@gmail.com>
2023-08-04 19:07:59 +0900

    MemberList(func): build Member struct

---
+++ MemberList/MemberList/Models/Member.swift
@@ -0,0 +1,35 @@
+//
+//  Member.swift
+//  MemberList
+//
+//  Created by 이민호 on 2023/08/04.
+//
+
+import UIKit
+
+struct Member {
+    lazy var memberImage: UIImage? = {
+        guard let name = name else {
+            return UIImage(systemName: "person")
+        }
+        return UIImage(named: "\(name).png") ?? UIImage(systemName: "person")
+    }()
+    
+    static var memberNumbers: Int = 0
+    
+    let id: Int
+    var name: String?
+    var age: Int?
+    var phone: String?
+    var address: String?
+    
+    init(name: String?, age: Int?, phone: String?, address: String?) {
+        self.id = Member.memberNumbers
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        
+        Member.memberNumbers += 1
+    }
+}
