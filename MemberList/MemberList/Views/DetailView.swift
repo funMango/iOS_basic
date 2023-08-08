770c4110e41ea115f666dbf4c8c418eda3731510
funMango <bluemango608@gmail.com>
2023-08-08 14:12:28 +0900

    MemberList(layout): build constraints

--- MemberList/MemberList/Views/DetailView.swift
+++ MemberList/MemberList/Views/DetailView.swift
@@ -185,4 +185,64 @@ class DetailView: UIView {
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
+    
+    // 레이블 넓이 저장을 위한 속성
+    let labelWidth: CGFloat = 70
+    // 애니메이션을 위한 속성 선언
+    var stackViewTopConstraint: NSLayoutConstraint!
+    
+    //MARK: - 생성자 셋팅
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupStackView()        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupStackView() {
+        self.addSubview(stackView)
+    }
+    
+    //MARK: - 오토레이아웃 셋팅
+    
+    // 오토레이아웃 업데이트
+    override func updateConstraints() {
+        setConstraints()
+        super.updateConstraints()
+    }
+    
+    func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            mainImageView.heightAnchor.constraint(equalToConstant: 150),
+            mainImageView.widthAnchor.constraint(equalToConstant: 150),
+            mainImageView.centerXAnchor.constraint(equalTo: imgContainView.centerXAnchor),
+            mainImageView.centerYAnchor.constraint(equalTo: imgContainView.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            imgContainView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memberIdLabel.widthAnchor.constraint(equalToConstant: labelWidth),
+            nameLabel.widthAnchor.constraint(equalToConstant: labelWidth),
+            ageLabel.widthAnchor.constraint(equalToConstant: labelWidth),
+            phoneNumberLabel.widthAnchor.constraint(equalToConstant: labelWidth),
+            addressLabel.widthAnchor.constraint(equalToConstant: labelWidth)
+        ])
+        
+        stackViewTopConstraint = stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10)
+        
+        NSLayoutConstraint.activate([
+            stackViewTopConstraint,
+            stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        ])
+    }
 }
