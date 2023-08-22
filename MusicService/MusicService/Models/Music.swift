014f2be02cdb4e0620b153b80ed322804265c561
funMango <bluemango608@gmail.com>
2023-08-22 13:14:11 +0900

    MusicService: build Music model

---
+++ MusicService/MusicService/Models/Music.swift
@@ -0,0 +1,14 @@
+//
+//  Music.swift
+//  MusicService
+//
+//  Created by 이민호 on 2023/08/22.
+//
+
+import UIKit
+
+struct Movie {
+    var img: UIImage?
+    let name: String
+    let description: String
+}
