566ce5e357c20569405c4678fbddaf3fa500a40f
funMango <bluemango608@gmail.com>
2023-08-28 16:33:11 +0900

    MusicService: build music cell

---
+++ MusicService/MusicService/Views/MyMusicCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyMusicCell.swift
+//  MusicService
+//
+//  Created by 이민호 on 2023/08/28.
+//
+
+import UIKit
+
+class MyMusicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
