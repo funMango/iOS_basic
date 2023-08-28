896489e61486781a5921f82a94272e3e83acb9ca
funMango <bluemango608@gmail.com>
2023-08-28 16:37:01 +0900

    MusicService: build constant

--- MusicService/MusicService/Views/MyMusicCell.swift
+++ MusicService/MusicService/Views/MyMusicCell.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class MyMusicCell: UITableViewCell {
 
+    @IBOutlet var img_main: UIImageView!
+    
+    @IBOutlet var lbl_songName: UILabel!
+    
+    @IBOutlet var lbl_artistName: UILabel!
+    
+    @IBOutlet var lbl_albumName: UILabel!
+    
+    @IBOutlet var lbl_releaseDate: UILabel!    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
